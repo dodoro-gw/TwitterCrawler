c2ebe9a8898091704fb298f9e5d77c94196b4b4e
taosbeta <stanley.suntao@gmail.com>
2014-11-29 22:36:00 +0800

    Refactor CrawlerProcessor

--- src/main/java/sg/edu/smu/sis/twittercrawler/util/UserIdReader.java
+++ src/main/java/sg/edu/smu/sis/twittercrawler/util/UserIdReader.java
@@ -1,38 +1,44 @@
 package sg.edu.smu.sis.twittercrawler.util;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserIdReader {
-	
-	//for loading user id list
-	public static long[] getUserList(String userIdFile)
-			throws FileNotFoundException, IOException {
-		//get user list
-		BufferedReader reader = new BufferedReader(new FileReader(userIdFile));
-		String line = null;
-		List<Long> userIds = new ArrayList<Long>();
-		while((line = reader.readLine()) != null){
-			String id = line;
-			if(id.length() < 1){
-				continue;
-			}
-			if(line.contains(",")){
-				String columns[] = line.split(",");
-				id = columns[0];
-			}
-			
-			userIds.add(Long.valueOf(id));
-		}
-		long[] usersList = new long[userIds.size()];
-		for(int i = 0; i < userIds.size(); i++){
-			usersList[i] = userIds.get(i);
-		}
-		reader.close();
-		return usersList;
-	}
+
+    public static List<Long> getUsers(String userIdFile) throws IOException {
+        //get user list
+        BufferedReader reader = new BufferedReader(new FileReader(userIdFile));
+        String line;
+        List<Long> userIds = new ArrayList<>();
+        while ((line = reader.readLine()) != null) {
+            String id = line;
+            if (id.length() < 1) {
+                continue;
+            }
+            if (line.contains(",")) {
+                String columns[] = line.split(",");
+                id = columns[0];
+            }
+
+            userIds.add(Long.valueOf(id));
+        }
+        reader.close();
+
+        return userIds;
+    }
+
+    //for loading user id list
+    public static long[] getUserList(String userIdFile)
+            throws IOException {
+        List<Long> userIds = getUsers(userIdFile);
+
+        long[] usersList = new long[userIds.size()];
+        for (int i = 0; i < userIds.size(); i++) {
+            usersList[i] = userIds.get(i);
+        }
+        return usersList;
+    }
 }
